689d56e96cc7a81a96d0a27c430c411af731c03c
Tera Kirk <terakrk@gmail.com>
2023-07-07 21:17:43 -0500

    wrote prompt/playerSelection

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 "use strict"
-
+const playerSelection = prompt("Rock, paper or scissors?").toLowerCase();
+const computerSelection = getComputerChoice();
 
 function getComputerChoice(min, max) {
     min = 1;
     max = 3;
     return Math.floor(Math.random() * (max) + min);
 }
+
+
+
+function playRound(playerSelection, computerSelection) {
+    return { playerSelection, computerSelection };
+    
+
+}
+
+console.log(playerSelection);
+console.log(computerSelection);
