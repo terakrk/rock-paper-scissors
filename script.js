6f6b5d80b3dbb6752562db8d333d49192985095a
Tera Kirk <terakrk@gmail.com>
2023-07-17 12:56:31 -0500

    plays 5 rounds, declares outcome of each

--- script.js
+++ script.js
@@ -1,92 +1,74 @@
 "use strict";
 
+function getPlayerChoice() {
+    let playerSelection = prompt('Rock, paper or scissors?').toLowerCase();
+    return playerSelection;
+}
+
+function getComputerChoice() {
+    let choices = Math.floor(Math.random()*3 +1);
+    switch(choices) {
+        case 1:
+            return 'rock';
+        case 2:
+            return 'paper';
+        case 3: 
+        return 'scissors';
+            
+    }
+}
+
+function game() {
+    for (let i = 1; i <= 5; i++) {
+        let computerSelection = getComputerChoice();
+        let playerSelection = getPlayerChoice();
+        let outcome = playRound(playerSelection, computerSelection);
+        let playerScore = 0;
+        let computerScore = 0;
+        let tie = 0;
+        playRound();
+        if (outcome.includes("You win!")) {
+            playerScore++;
+        } else if (outcome.includes("You lose!")) {
+            computerScore++;
+        } else {
+            tie++;
+    } 
+
+console.log(`Player: ${playerSelection}; Computer: ${computerSelection}. ${outcome} `);
+    }
+
+    switch(true) {
+        case (playerScore > computerScore) && (playerScore > tie):
+            return "Congratulations! You win!";
+        case (playerScore < computerScore) && (computerScore > tie):
+            return "Sorry! You lose!";
+        case (tie < playerScore) && (tie < computerScore):
+            return "It's a tie!";
+    }
+}   
 
 
 function playRound(playerSelection, computerSelection) {
-
-    switch (true) {
-       case playerSelection == "rock" && computerSelection == "scissors":
-             return "Rock beats scissors! You win!";
-   
-       case playerSelection == "paper" && computerSelection == "rock":
-             return "Paper beats rock! You win!";
-   
-       case playerSelection == "scissors" && computerSelection == "paper":
-               return "Scissors beats paper! You win!";
-   
-       case playerSelection == "rock" && computerSelection == "paper":
-             return "Paper beats rock! You lose!";
-   
-       case playerSelection == "paper" && computerSelection == "scissors":
-             return "Scissors beats paper! You lose!";
-   
-       case playerSelection == "scissors" && computerSelection == "rock":
-               return "Rock beats scissors! You lose!";
-       default:
-                  return "It's a tie!";
-           }
-
-                
+    
+    if ((playerSelection == 'rock') && (computerSelection == 'scissors')) {
+       return "You win! Rock beats scissors!";
+    } else if ((playerSelection == 'paper') && (computerSelection == 'rock')) {
+        return "You win! Paper beats rock!";
+    } else if ((playerSelection == 'scissors') && (computerSelection == 'paper')) {
+       return "You win! Scissors beats paper!";
+    } else if ((playerSelection == 'rock' && computerSelection == 'paper')) {
+        return "You lose! Paper beats rock!";
+    } else if ((playerSelection == 'paper') && (computerSelection == 'scissors')) {
+       return "You lose! Scissors beats paper!";
+    } else if ((playerSelection == 'scissors') && (computerSelection == 'rock')) {
+        return "You lose! Rock beats scissors!";
+    }  else {
+        return "It's a tie!";
+    }
    }
 
-   let playerSelection = prompt('Rock, paper or scissors?').toLowerCase();
-let computerSelection = getComputerChoice();
+    
+    
    
-
-
-function getComputerChoice(min, max) {
-    min = 1;
-    max = 3;
-    return Math.floor(Math.random() * (max) + min);
-}
-
-switch (computerSelection)  {
-    case 1:
-    computerSelection = "rock";
-    break;
-    case 2:
-     computerSelection = "paper";
-     break;
-    case 3:
-        computerSelection == "scissors";
-        break;
-        
-}
-
-let roundResult = playRound(playerSelection, computerSelection); 
-alert(`Player choice: ${playerSelection}; Computer choice: ${computerSelection}.${roundResult}`);
-
-
-let playerScore = 0;
-let computerScore = 0;
-let tie = 0;
-
-   
-
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt('Rock, paper or scissors?').toLowerCase();
-let computerSelection = getComputerChoice();
-        playRound(playerSelection, computerSelection);
-    }
-
-    if (roundResult.includes("You win!")) {
-        playerScore++;
-    } else if (roundResult.includes("You lose!")) {
-        computerScore++;
-        } else {
-            tie++;
-        }
-}
-    switch (true) {
-        case (playerScore > computerScore) && (playerScore > tie):
-            alert("Congratulations! You win!");
-        case (computerScore > playerScore) && (computerScore > tie):
-             alert("Sorry! I win!");
-            case playerScore == computerScore:
-                alert("It's a tie!");
-    }
-
-
-
 game();
